597212146b5843ae4205f90d417007211951b76c
nispar2009 <varun.parupalli@gmail.com>
2023-01-16 20:38:12 +0530

    Sign out finished

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ cursor = connection.cursor()
 
 cursor.execute('CREATE TABLE IF NOT EXISTS users (username text, password text, balance int)')
 
+connection.commit()
+connection.close()
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     connection = sqlite3.connect('moneymaster.db')
@@ -21,7 +24,7 @@ def index():
 
         if login['user_exists']:
             if login['auth']:
-                response = make_response('You are logged in to your account')
+                response = make_response('You are logged in to your account. Click <a href="/">here</a>.')
                 response.set_cookie('moneymaster_user', username)
                 return response
             else:
@@ -29,15 +32,25 @@ def index():
 
         cursor.execute('INSERT INTO users (username, password, balance) VALUES (?, ?, 0)', (username, password))
         connection.commit()
-        response = make_response('Your new account has been created.')
+        response = make_response('Your new account has been created. Click <a href="/">here</a>.')
         response.set_cookie('moneymaster_user', username)
         return response
 
     print(request.cookies.get('moneymaster_user'))
+
+    connection.commit()
     
     if len(list(cursor.execute('SELECT * FROM users WHERE username=?', (request.cookies.get('moneymaster_user'),)))) > 0:
-        return render_template('index.html', user=list(cursor.execute('SELECT * FROM users WHERE username=?', (request.cookies.get('moneymaster_user'),)))[0])
+        return render_template('index.html', user=request.cookies.get('moneymaster_user'))
     
     return render_template('login.html')
 
-app.run(debug=True)
+@app.route('/signout')
+def signout():
+    response = make_response(f'You are signed out from your previous account ({request.cookies.get("moneymaster_user")}). Click <a href="/">here</a> to log on from another account.')
+    response.set_cookie('moneymaster_user', '')
+    return response
+
+
+
+app.run(debug=True, port='5000')
