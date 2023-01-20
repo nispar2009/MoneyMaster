a26ef7db4a84039f00bf67f12d87d2949c47deb0
nispar2009 <varun.parupalli@gmail.com>
2023-01-20 12:15:45 +0530

    Complete code

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, redirect, request, make_response
+from flask import Flask, render_template, redirect, request
 import sqlite3
-import datetime
 
 app = Flask(__name__)
 
@@ -10,7 +9,7 @@ def index():
     cursor = connection.cursor()
     
     if len(list(cursor.execute('SELECT * FROM users WHERE username=?', (request.cookies.get('cashco_user'),)))) > 0:
-        return render_template('index.html', user=request.cookies.get('cashco_user'))
+        return render_template('index.html', user=request.cookies.get('cashco_user'), balance=(list(cursor.execute('SELECT * FROM users WHERE username=?', (request.cookies.get('cashco_user'),))))[0][2])
     
     return redirect('http://127.0.0.1:5000')
 
@@ -19,19 +18,22 @@ def transfer():
     connection = sqlite3.connect('cashco.db')
     cursor = connection.cursor()
 
-    money = request.form['money']
+    money = int(request.form['money'])
     fr = request.cookies.get('cashco_user')
     to = request.form['to']
 
-    if (len(list(cursor.execute('SELECT * FROM users WHERE username=?', fr))) == 1) and (len(list(cursor.execute('SELECT * FROM users WHERE username=?', to))) == 1):
-        cursor.execute('UPDATE users WHERE username=? SET balance=balance+?', (to, money))
-        cursor.execute('UPDATE users WHERE username=? SET balance=balance-?', (fr, money))
-        cursor.execute('INSERT INTO msg (feature, receiver, cnt, date) VALUES (?, ?, ?, ?)', (5001, to, f'Dear user, {fr} has transferred to you an amount of ${money}.', datetime.datetime().utcnow()))
+    if (len(list(cursor.execute('SELECT * FROM users WHERE username=?', (fr,)))) == 1) and (len(list(cursor.execute('SELECT * FROM users WHERE username=?', (to,)))) == 1):
+        cursor.execute('UPDATE users SET balance=balance+? WHERE username=?', (money, to))
+        cursor.execute('UPDATE users SET balance=balance-? WHERE username=?', (money, fr))
+        cursor.execute('INSERT INTO msg (feature, receiver, cnt) VALUES (5001, ?, ?)', (to, (f'Dear user, {fr} has transferred to you an amount of ${money}.')))
+        cursor.execute('INSERT INTO msg (feature, receiver, cnt) VALUES (5001, ?, ?)', (fr, (f'Dear user, an amount of ${money} has been transferred from your account to {to}.')))
+        print(list(cursor.execute('SELECT * FROM msg')))   
+        connection.commit()
+        connection.close()
     else:
-        return 'Transaction failed. Make sure you are <a href="http://127.0.0.1:5000>logged in</a> and that the recipient of your money exists.'
+        return 'Transaction failed.'
 
-    connection.commit()
-    connection.close()
+    return redirect('/')
 
 @app.route('/msg')
 def msg():
@@ -39,11 +41,11 @@ def msg():
     cursor = connection.cursor()
     
     if len(list(cursor.execute('SELECT * FROM users WHERE username=?', (request.cookies.get('cashco_user'),)))) > 0:
-        all_msg = cursor.execute('SELECT cnt FROM msg WHERE receiver=? AND feature=5001', ((request.cookies.get('cashco_user')),))
-        return render_template('msg.html', msg=list(all_msg), len=len)
-
+        all_msg = list(cursor.execute('SELECT * FROM msg WHERE feature=5001 AND receiver=?', ((request.cookies.get('cashco_user')),)))
+        print(all_msg)
+        print(list(cursor.execute('SELECT * FROM msg')))
+        return render_template('msg.html', all_msg=all_msg, len1=len)
     connection.close()
-    
     return redirect('http://127.0.0.1:5000')
 
 app.run(debug=True, port='5001')
